dbbc357910d9e8f1cd9e4b9ca575f1b4f1fecdd0
Tim Zwolak <timothypage@gmail.com>
2023-09-08 09:11:34 -0600

    a little magic to make maputnik styling work well

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import 'maplibre-gl/dist/maplibre-gl.css'
 import './style.css'
 
-import maplibregl from 'maplibre-gl' // or "const maplibregl = require('maplibre-gl');"
+import maplibregl from 'maplibre-gl'
 import * as pmtiles from 'pmtiles'
+
+import bright from './map_styles/bright.json' // reload on change
 
 let protocol = new pmtiles.Protocol()
 maplibregl.addProtocol('pmtiles', protocol.tile)
@@ -13,7 +15,32 @@ const map = new maplibregl.Map({
   // style: 'https://demotiles.maplibre.org/style.json', // stylesheet location
   center: [-106, 39], // starting position [lng, lat]
   zoom: 6, // starting zoom
-  style: '/map_styles/bright.json'
+  style: {
+    version: 8,
+    name: 'Bright',
+    metadata: {},
+    center: [-106, 39],
+    zoom: 6,
+    bearing: 0,
+    pitch: 0,
+    sources: {
+      openmaptiles: {
+        type: 'vector',
+        url: 'pmtiles:///tiles/us.pmtiles'
+      },
+      usfs_national_forests_and_grasslands: {
+        type: 'vector',
+        url: 'pmtiles:///tiles/usfs_national_forests_and_grasslands.pmtiles'
+      },
+      us_federal_proclaimed_areas: {
+        type: 'vector',
+        url: 'pmtiles:///tiles/us_federal_proclaimed_areas.pmtiles'
+      }
+    },
+    sprite: 'http://localhost:5173/map_styles/sprite',
+    glyphs: '/fonts/{fontstack}/{range}.pbf',
+    layers: [...bright.layers]
+  }
 })
 
 map.addControl(
@@ -38,6 +65,6 @@ map.addControl(
   new maplibregl.ScaleControl({
     maxWidth: 200,
     unit: 'imperial'
-}),
+  }),
   'bottom-left'
 )
